12c81b4da8f3f4825687441fcd1304eae0debd33
[comet3321] <[apd.jinx@gmail.com]>
2017-08-23 05:49:47 +0900

    add login function

--- admin.php
+++ admin.php
@@ -3,29 +3,47 @@
 require_once('config.php');
 
 //もしログインに失敗した場合はログインページへ飛ばす
+try {
+  $pdo = new PDO(DSN, DB_USERNAME, DB_PASSWORD);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+  echo $e->getMessage() . PHP_EOL;
+}
 
-$stmt = $this->db->prepare("select * from admin where email = :email");
- $stmt->execute([
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $stmt = $pdo->prepare("select * from admin where email = :email");
+  $stmt->execute([
    ':email' => $_POST['email']
- ]);
- $stmt->setFetchMode(\PDO::FETCH_CLASS, 'stdClass');
- $user = $stmt->fetch();
+  ]);
+  $stmt->setFetchMode(\PDO::FETCH_CLASS, 'stdClass');
+  $user = $stmt->fetch();
 
- if (empty($user)) {
-   echo '失敗';
-   exit()
- }
+  if (empty($user)) {
+   echo 'ログイン失敗';
+   exit();
+  }
 
- if (!password_verify($_POST['password'], $user->password)) {
-   echo '失敗'
- }
-
+  //if (!password_verify($_POST['password'], $user->password)) {
+  if(!$_POST['password'] == $user->password){ //登録画面を作るまで
+   echo 'ログイン失敗';
+   exit();
+  }
+}
 //AdminUserの場合新規Userの追加ページを表示
-
-//一般Userの場合
 
 //前回のログイン以降の予約を表示
 
 //1週間ごとの詳しい予約内容を表示
 
  ?>
+
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>管理ページ</title>
+   </head>
+   <body>
+     <p>ようこそ<?= $user->name; ?>さん</p>
+   </body>
+ </html>
